88a6fbbc2b1205a9516441cb32c63eb160c88089
Fabrice Atlan <fabrice.atlan.56@gmail.com>
2024-10-29 17:23:29 +0100

    Create new branch state.1 and add 2 buttons for change pokemon. Work in progress.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
@@ -8,14 +9,57 @@ const pokemonList = [
 			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
 	},
 	{
+		name: "charmander",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+	},
+	{
+		name: "squirtle",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+	},
+	{
+		name: "pikachu",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+	},
+	{
 		name: "mew",
 	},
 ];
 
 function App() {
+	const pokemonIndex = useState(pokemonList);
+	const [index, setIndex] = useState(0);
+
+	console.log(pokemonIndex[0]);
+
+	function handleClickPrev() {
+		if (index > 0) {
+			setIndex(index - 1);
+		}
+	}
+
+	function handleClickNext() {
+		if (index < pokemonList.length - 1) {
+			setIndex(index + 1);
+		}
+	}
+
 	return (
 		<>
-			<section>{<PokemonCard pokemon={pokemonList[0]} />}</section>
+			<section>
+				{<PokemonCard pokemon={pokemonList[index]} />}
+				<div className="btn-container">
+					<button className="btn" type="button" onClick={handleClickPrev}>
+						Précédent
+					</button>
+
+					<button className="btn" type="button" onClick={handleClickNext}>
+						Suivant
+					</button>
+				</div>
+			</section>
 		</>
 	);
 }
